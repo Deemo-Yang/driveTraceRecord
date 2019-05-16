f6adb44465060edece20991aa5b5c3ffc4ec84a3
Deemo-Yang <ywy0711@outlook.com>
2019-05-16 11:22:26 +0800

    falconService

--- DriveTraceRecord/app/src/main/java/com/example/drivetracerecord/MainActivity.java
+++ DriveTraceRecord/app/src/main/java/com/example/drivetracerecord/MainActivity.java
@@ -2,14 +2,24 @@ package com.example.drivetracerecord;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.nfc.Tag;
+import android.os.Build;
+import android.provider.SyncStateContract;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
@@ -27,6 +37,16 @@ import com.amap.api.maps.model.MyLocationStyle;
 import com.amap.api.maps.model.Polyline;
 import com.amap.api.maps.model.PolylineOptions;
 import com.amap.api.trace.TraceLocation;
+import com.amap.api.track.AMapTrackClient;
+import com.amap.api.track.ErrorCode;
+import com.amap.api.track.OnTrackLifecycleListener;
+import com.amap.api.track.TrackParam;
+import com.amap.api.track.query.model.AddTerminalRequest;
+import com.amap.api.track.query.model.AddTerminalResponse;
+import com.amap.api.track.query.model.AddTrackRequest;
+import com.amap.api.track.query.model.AddTrackResponse;
+import com.amap.api.track.query.model.QueryTerminalRequest;
+import com.amap.api.track.query.model.QueryTerminalResponse;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -53,6 +73,18 @@ public class MainActivity extends AppCompatActivity implements LocationSource,AM
     private long mEndTime;
     private List<TraceLocation> mTracelocationlist = new ArrayList<TraceLocation>();
 
+    private AMapTrackClient aMapTrackClient;
+    private static final String TAG = "TrackServiceActivity";
+    private static final String CHANNEL_ID_SERVICE_RUNNING = "CHANNEL_ID_SERVICE_RUNNING";
+
+    private boolean isServiceRunning;
+    private boolean isGatherRunning;
+    private TextView startTrackView;
+    private TextView startGatherView;
+    private long terminalId;
+    private long trackId;
+    private boolean uploadToTrack = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -74,6 +106,34 @@ public class MainActivity extends AppCompatActivity implements LocationSource,AM
                     record.setDate(getcueDate(mStartTime)); //记录开始时间
                 }else {
                     mEndTime = System.currentTimeMillis();  //记录结束时间
+                }
+            }
+        });
+
+        aMapTrackClient = new AMapTrackClient(getApplicationContext());
+        aMapTrackClient.setInterval(2, 20);
+        startTrackView = findViewById(R.id.activity_track_service_start_track);
+        startGatherView = findViewById(R.id.activity_track_service_start_gather);
+        updateBtnStatus();
+        startTrackView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isServiceRunning) {
+                    aMapTrackClient.stopTrack(new TrackParam(Constants.SERVICE_ID, terminalId), onTrackListener);
+                } else {
+                    startTrack();
+                }
+            }
+        });
+        // 采集启停
+        startGatherView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isGatherRunning) {
+                    aMapTrackClient.stopGather(onTrackListener);
+                } else {
+                    aMapTrackClient.setTrackId(trackId);
+                    aMapTrackClient.startGather(onTrackListener);
                 }
             }
         });
@@ -174,6 +234,170 @@ public class MainActivity extends AppCompatActivity implements LocationSource,AM
         String date = formatter.format(curDate);
         return date;
     }
+
+    private OnTrackLifecycleListener onTrackListener = new SimpleOnTrackLifecycleListener() {
+        @Override
+        public void onBindServiceCallback(int status, String msg) {
+            Log.w(TAG, "onBindServiceCallback, status: " + status + ", msg: " + msg);
+        }
+
+        @Override
+        public void onStartTrackCallback(int status, String msg) {
+            if (status == ErrorCode.TrackListen.START_TRACK_SUCEE || status == ErrorCode.TrackListen.START_TRACK_SUCEE_NO_NETWORK) {
+                // 成功启动
+                Toast.makeText(MainActivity.this, "启动服务成功", Toast.LENGTH_SHORT).show();
+                isServiceRunning = true;
+                updateBtnStatus();
+            } else if (status == ErrorCode.TrackListen.START_TRACK_ALREADY_STARTED) {
+                // 已经启动
+                Toast.makeText(MainActivity.this, "服务已经启动", Toast.LENGTH_SHORT).show();
+                isServiceRunning = true;
+                updateBtnStatus();
+            } else {
+                Log.w(TAG, "error onStartTrackCallback, status: " + status + ", msg: " + msg);
+                Toast.makeText(MainActivity.this,
+                        "error onStartTrackCallback, status: " + status + ", msg: " + msg,
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+        @Override
+        public void onStopTrackCallback(int status, String msg) {
+            if (status == ErrorCode.TrackListen.STOP_TRACK_SUCCE) {
+                // 成功停止
+                Toast.makeText(MainActivity.this, "停止服务成功", Toast.LENGTH_SHORT).show();
+                isServiceRunning = false;
+                isGatherRunning = false;
+                updateBtnStatus();
+            } else {
+                Log.w(TAG, "error onStopTrackCallback, status: " + status + ", msg: " + msg);
+                Toast.makeText(MainActivity.this,
+                        "error onStopTrackCallback, status: " + status + ", msg: " + msg,
+                        Toast.LENGTH_LONG).show();
+
+            }
+        }
+
+        @Override
+        public void onStartGatherCallback(int status, String msg) {
+            if (status == ErrorCode.TrackListen.START_GATHER_SUCEE) {
+                Toast.makeText(MainActivity.this, "定位采集开启成功", Toast.LENGTH_SHORT).show();
+                isGatherRunning = true;
+                updateBtnStatus();
+            } else if (status == ErrorCode.TrackListen.START_GATHER_ALREADY_STARTED) {
+                Toast.makeText(MainActivity.this, "定位采集已经开启", Toast.LENGTH_SHORT).show();
+                isGatherRunning = true;
+                updateBtnStatus();
+            } else {
+                Log.w(TAG, "error onStartGatherCallback, status: " + status + ", msg: " + msg);
+                Toast.makeText(MainActivity.this,
+                        "error onStartGatherCallback, status: " + status + ", msg: " + msg,
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+
+        @Override
+        public void onStopGatherCallback(int status, String msg) {
+            if (status == ErrorCode.TrackListen.STOP_GATHER_SUCCE) {
+                Toast.makeText(MainActivity.this, "定位采集停止成功", Toast.LENGTH_SHORT).show();
+                isGatherRunning = false;
+                updateBtnStatus();
+            } else {
+                Log.w(TAG, "error onStopGatherCallback, status: " + status + ", msg: " + msg);
+                Toast.makeText(MainActivity.this,
+                        "error onStopGatherCallback, status: " + status + ", msg: " + msg,
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+    };
+
+    private void updateBtnStatus() {
+        startTrackView.setText(isServiceRunning ? "停止服务" : "启动服务");
+        startTrackView.setTextColor(isServiceRunning ? 0xFFFFFFFF : 0xFF000000);
+        startGatherView.setText(isGatherRunning ? "停止采集" : "开始采集");
+        startGatherView.setTextColor(isGatherRunning ? 0xFFFFFFFF : 0xFF000000);
+    }
+
+    private void startTrack() {
+        // 先根据Terminal名称查询Terminal ID，如果Terminal还不存在，就尝试创建，拿到Terminal ID后，
+        // 用Terminal ID开启轨迹服务
+        aMapTrackClient.queryTerminal(new QueryTerminalRequest(Constants.SERVICE_ID, Constants.TERMINAL_NAME), new SimpleOnTrackListener() {
+            @Override
+            public void onQueryTerminalCallback(QueryTerminalResponse queryTerminalResponse) {
+                if (queryTerminalResponse.isSuccess()) {
+                    if (queryTerminalResponse.isTerminalExist()) {
+                        // 当前终端已经创建过，直接使用查询到的terminal id
+                        terminalId = queryTerminalResponse.getTid();
+                        if (uploadToTrack) {
+                            aMapTrackClient.addTrack(new AddTrackRequest(Constants.SERVICE_ID, terminalId), new SimpleOnTrackListener() {
+                                @Override
+                                public void onAddTrackCallback(AddTrackResponse addTrackResponse) {
+                                    if (addTrackResponse.isSuccess()) {
+                                        // trackId需要在启动服务后设置才能生效，因此这里不设置，而是在startGather之前设置了track id
+                                        trackId = addTrackResponse.getTrid();
+                                        TrackParam trackParam = new TrackParam(Constants.SERVICE_ID, terminalId);
+                                        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                                            trackParam.setNotification(createNotification());
+                                        }
+                                        aMapTrackClient.startTrack(trackParam, onTrackListener);
+                                    } else {
+                                        Toast.makeText(MainActivity.this, "网络请求失败，" + addTrackResponse.getErrorMsg(), Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                        } else {
+                            // 不指定track id，上报的轨迹点是该终端的散点轨迹
+                            TrackParam trackParam = new TrackParam(Constants.SERVICE_ID, terminalId);
+                            if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                                trackParam.setNotification(createNotification());
+                            }
+                            aMapTrackClient.startTrack(trackParam, onTrackListener);
+                        }
+                    } else {
+                        // 当前终端是新终端，还未创建过，创建该终端并使用新生成的terminal id
+                        aMapTrackClient.addTerminal(new AddTerminalRequest(Constants.TERMINAL_NAME, Constants.SERVICE_ID), new SimpleOnTrackListener() {
+                            @Override
+                            public void onCreateTerminalCallback(AddTerminalResponse addTerminalResponse) {
+                                if (addTerminalResponse.isSuccess()) {
+                                    terminalId = addTerminalResponse.getTid();
+                                    TrackParam trackParam = new TrackParam(Constants.SERVICE_ID, terminalId);
+                                    if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                                        trackParam.setNotification(createNotification());
+                                    }
+                                    aMapTrackClient.startTrack(trackParam, onTrackListener);
+                                } else {
+                                    Toast.makeText(MainActivity.this, "网络请求失败，" + addTerminalResponse.getErrorMsg(), Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "网络请求失败，" + queryTerminalResponse.getErrorMsg(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    private Notification createNotification() {
+        Notification.Builder builder;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID_SERVICE_RUNNING, "app service", NotificationManager.IMPORTANCE_LOW);
+            nm.createNotificationChannel(channel);
+            builder = new Notification.Builder(getApplicationContext(), CHANNEL_ID_SERVICE_RUNNING);
+        } else {
+            builder = new Notification.Builder(getApplicationContext());
+        }
+        Intent nfIntent = new Intent(MainActivity.this, MainActivity.class);
+        nfIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        builder.setContentIntent(PendingIntent.getActivity(MainActivity.this, 0, nfIntent, 0))
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("猎鹰sdk运行中")
+                .setContentText("猎鹰sdk运行中");
+        Notification notification = builder.build();
+        return notification;
+    }
+
 
     public void getPermission() {
         if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.
